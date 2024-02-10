8eb7f9d5776c82c58b5f6e76285ff09bb10f9e93
jech1 <jacob.echeverry13@gmail.com>
2024-02-10 15:08:23 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -5,14 +5,28 @@ const bodyParser = require('body-parser');
 const app = express();
 const port= 3000;
 
-// Importing the Router being used 
-const emailRouter = require('./routes/emailRouter');
-
 // This will be the middleware to parse JSON files
 app.use(bodyParser.json());
 
-//Will this come up as undefined? Check line 6 of emailRouter.js
-app.use('/genemails', emailRouter);
+// Route to the random emails // basically what was being done in the emailRouter.js
+app.post('/generate-emails', (req, res) => {
+// Extract the subject and body from the request body
+const { subject, body } = req.body;
+
+// Generate a random number (0 or 1) to determine if it's a phishing link or a legitimate link
+const isPhishing = Math.random() < 0.5;   
+
+// Construct the phishing email object
+const email = {
+    subject: subject,
+    body: body,
+    link: isPhishing ? 'http://phishing.example.com' : 'http://legitimate.example.com',
+    isPhishing: isPhishing
+};
+
+//this is what will be returned
+res.json(email);
+});
 
 //This will work as a default route
 app.get('/', (req, res) => {
