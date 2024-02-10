598ee39d46f2a4c736d41fb766a9c614821cbb2c
segfaultboi@gmail.com <segfaultboi@gmail.com>
2024-02-09 23:01:54 -0500

    new commit for win_screen

---
+++ client/src/pages/win_screen.jsx
@@ -0,0 +1,11 @@
+const winScreen = () => {
+    return (
+        <div>
+        <h1>You Win!</h1>
+        <Link to="/">Play Again</Link>
+        </div>
+    );
+    }
+
+
+export default winScreen;
